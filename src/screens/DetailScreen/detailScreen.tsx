2ef8b1f4ece844747f20b7a105fc4609bba390dc
Nikita <nikita.tikhonov@viaccess-orca.com>
2023-06-15 17:35:41 +0300

    fix some linting issues

--- src/screens/DetailScreen/detailScreen.tsx
+++ src/screens/DetailScreen/detailScreen.tsx
@@ -1,11 +1,18 @@
 import {Text} from 'react-native'
 import * as React from 'react'
 import {BaseView} from 'theme'
+import styled from 'styled-components/native'
 
 export const DetailsScreen = () => {
   return (
-    <BaseView style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <DetailsView>
       <Text>Details Screen</Text>
-    </BaseView>
+    </DetailsView>
   )
 }
+
+const DetailsView = styled(BaseView)({
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+})
